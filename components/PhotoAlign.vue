771020fd49a6f5ff5b7f2f612c0f2fb3373d428c
Santiago Toyos <ppikachu@gmail.com>
2024-05-11 03:28:03 -0300

    Update PhotoAlign.vue

--- components/PhotoAlign.vue
+++ components/PhotoAlign.vue
@@ -7,19 +7,19 @@ const model: any = defineModel()
     <UButtonGroup :ui="{ wrapper: { horizontal: 'grid grid-cols-3'} }">
       <UButton
         @click="model = 'object-right-top'"
-        icon="i-heroicons-bars-3-bottom-right-16-solid"
+        icon="i-mdi-arrow-collapse-down"
         block
         :class="model === 'object-right-top' ? 'opacity-100' : 'opacity-50'"
       />
       <UButton
         @click="model = 'object-center'"
-        icon="i-heroicons-bars-3-16-solid"
+        icon="i-mdi-arrow-collapse-vertical"
         block
         :class="model === 'object-center' ? 'opacity-100' : 'opacity-50'"
       />
       <UButton
         @click="model = 'object-left-bottom'"
-        icon="i-heroicons-bars-3-bottom-left-16-solid"
+        icon="i-mdi-arrow-collapse-up"
         block
         :class="model === 'object-left-bottom' ? 'opacity-100' : 'opacity-50'"
       />
