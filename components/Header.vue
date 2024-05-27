50b6682481dfe9d90c57970d5657855e009333fa
Santiago Toyos <ppikachu@gmail.com>
2024-05-27 10:35:45 -0300

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -49,29 +49,28 @@ async function resetSettings() {
         <UButton icon="i-heroicons-question-mark-circle-16-solid" color="gray" variant="link" size="lg" @click="isOpen = true"/>
         <UModal v-model="isOpen" :ui="{ container: 'items-center sm:items-center' }" >
           <UCard>
-            <h3 class="text-center text-primary mb-4 text-xl">{{ $t('help') }}</h3>
-            <p class="prose dark:prose-invert mb-4">{{ $t('description') }}</p>
-            <UButton icon="i-mdi-close" @click="isOpen = false" class="mb-8 absolute top-2 sm:top-4 right-2 sm:right-4"/>
-
-            <UAccordion :items="items" size="lg">
-              <template #item="{ item }">
-                <div v-html="item.content" class="prose dark:prose-invert prose-sm"/>
-              </template>
-              <template #reset>
-                <div class="text-sm prose dark:prose-invert">
-                  <p>{{ $t('reset_to_default') }}</p>
-                  <UButton icon="i-mdi-refresh" label="Reset" @click="resetSettings" :block="ismobile.isMobileOrTablet" />
-                </div>
-              </template>
-            </UAccordion>
-
-            <UAlert icon="i-mdi-alert-circle" color="green" class="my-4">
-              <template #description>
-                <span class="italic text-xs">{{ $t('disclosure') }}</span>😎
-              </template>
-            </UAlert />
-
-            <a href="https://toyos.vercel.app" target="_blank" class="block text-xs text-center py-2 text-gray-500">Developed by TOYOS - {{ new Date().getFullYear() }}</a>
+            <section class="app-font">
+              <h3 class="text-center text-primary mb-4 text-xl">{{ $t('help') }}</h3>
+              <p class="prose dark:prose-invert mb-4">{{ $t('description') }}</p>
+              <UButton icon="i-mdi-close" @click="isOpen = false" class="mb-8 absolute top-2 sm:top-4 right-2 sm:right-4"/>
+              <UAccordion :items="items" size="lg">
+                <template #item="{ item }">
+                  <div v-html="item.content" class="prose dark:prose-invert prose-sm"/>
+                </template>
+                <template #reset>
+                  <div class="text-sm prose dark:prose-invert">
+                    <p>{{ $t('reset_to_default') }}</p>
+                    <UButton icon="i-mdi-refresh" label="Reset" @click="resetSettings" :block="ismobile.isMobileOrTablet" />
+                  </div>
+                </template>
+              </UAccordion>
+              <UAlert icon="i-mdi-alert-circle" color="green" class="my-4">
+                <template #description>
+                  <span class="italic text-xs">{{ $t('disclosure') }}</span>😎
+                </template>
+              </UAlert />
+              <a href="https://toyos.vercel.app" target="_blank" class="block text-xs text-center py-2 text-gray-500">Developed by TOYOS - {{ new Date().getFullYear() }}</a>
+            </section>
           </UCard>
         </UModal>
       </div>
